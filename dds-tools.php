1c27f0b99aea2b9c8670cd9e4f12888bace6f1bd
Younes Benkheil <younesbenkheil@gmail.com>
2021-05-25 19:34:23 +0200

    update

--- dds-tools.php
+++ dds-tools.php
@@ -4,7 +4,7 @@
 Plugin Name: Digiflow DDS Tools
 Plugin URI: https://github.com/younesben99/dds-tools
 Description: Tools for DDS website.
-Version: 0.80
+Version: 0.85
 Author: Younes Benkheil
 Author URI: https://digiflow.be/
 License: GPL2
--- modules/tracking_codes/bing_analytics.php
+++ modules/tracking_codes/bing_analytics.php
@@ -1,10 +1,11 @@
 <?php
 
 add_action( 'wp_head', function(){
-    //$digiflow_settings_options = get_option( 'digiflow_settings_option_name' );
-    //$digiflow_settings_options['bing_tracking_id_2']
+    $digiflow_settings_options = get_option( 'digiflow_settings_option_name' );
+    
     ?>
 
+<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"<?php echo($digiflow_settings_options['bing_tracking_id_2']); ?>"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
 
 
    <?php
