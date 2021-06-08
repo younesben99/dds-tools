bd50c199e67b944edca9e9b4ea3ac8e7dfbae963
Younes Benkheil <younesbenkheil@gmail.com>
2021-06-08 14:24:38 +0200

    update

--- assets/js/analytics_tracker.js
+++ assets/js/analytics_tracker.js
@@ -1,40 +1,40 @@
 jQuery(document).ready(function(){
     jQuery('a,button,input[type=submit]').on('click',function(){
    
-    var eventaction = 'None';
     var eventlabel = 'None';
+    var eventaction = 'None';
     var eventform = '';
     if(jQuery(this).attr('href')){
     if(jQuery(this).attr('href') !== ''){
-    eventaction = jQuery(this).attr('href');
+    eventlabel = jQuery(this).attr('href');
     }
     }
     else{
     if(jQuery(this).parents('form').length == 1 ){
         if(jQuery(this).parents('form').attr('name')){
-            eventaction = 'Form: ' + jQuery(this).parents('form').attr('name');
+            eventlabel = 'Form: ' + jQuery(this).parents('form').attr('name');
             eventform = jQuery(this).parents('form').attr('name') + ' > ';
         }
         else{
-            eventaction = 'Form: ' + jQuery(this).parents('form').attr('action');
+            eventlabel = 'Form: ' + jQuery(this).parents('form').attr('action');
             eventform = jQuery(this).parents('form').attr('action') + ' > ';
         }
     }
     }
     
     if(jQuery(this).text() !== ''){
-    eventlabel = jQuery(this).text().trim();
+    eventaction = jQuery(this).text().trim();
     }
     else{
-    eventlabel = jQuery(this).val().trim();
+    eventaction = jQuery(this).val().trim();
     }
     
-    console.log('eventlabel: ' +  eventform + eventlabel);
+    console.log('eventaction: ' +  eventform + eventaction);
     console.log('category: ' + window.location.href);
-    console.log('eventaction: ' + eventaction);
-  gtag('event', eventform + eventlabel, {
+    console.log('eventlabel: ' + eventlabel);
+  gtag('event', eventform + eventaction, {
           'event_category': window.location.href,
-          'event_label': eventaction
+          'event_label': eventlabel
     });
     
     });
