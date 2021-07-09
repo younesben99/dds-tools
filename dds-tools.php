77ad3909464099f5637bbd802aeee35b25d7664c
Younes Benkheil <younesbenkheil@gmail.com>
2021-07-09 02:46:53 +0200

    update

--- dds-tools.php
+++ dds-tools.php
@@ -1,12 +1,12 @@
 <?php
 
-$dds_version = "1.69";
+$dds_version = "1.70";
 
 /*
 Plugin Name: Digiflow DDS Tools
 Plugin URI: https://github.com/younesben99/dds-tools
 Description: Tools for DDS website.
-Version: 1.69
+Version: 1.70
 Author: Younes Benkheil
 Author URI: https://digiflow.be/
 License: GPL2
