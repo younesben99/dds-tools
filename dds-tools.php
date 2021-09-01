7910f6b1335d16d59b3f0593ff222d40cb53b72b
Younes Benkheil <younesbenkheil@gmail.com>
2021-09-01 23:12:05 +0200

    none

--- dds-tools.php
+++ dds-tools.php
@@ -1,12 +1,12 @@
 <?php
 
-$dds_version = "3.8.1";
+$dds_version = "3.8.2";
 
 /*
 Plugin Name: Digiflow DDS Tools
 Plugin URI: https://github.com/younesben99/dds-tools
 Description: Tools for DDS website.
-Version: 3.8.1
+Version: 3.8.2
 Author: Younes Benkheil
 Author URI: https://digiflow.be/
 License: GPL2
--- modules/tracking_codes/google_analytics.php
+++ modules/tracking_codes/google_analytics.php
@@ -22,7 +22,7 @@ add_action( 'wp_head', function(){
 
    wp_enqueue_script(
     'analytics_tracker', 
-    get_site_url() . '/wp-content/plugins/dds-tools/assets/js/analytics_tracker.js?v=1.5', 
+    get_site_url() . '/wp-content/plugins/dds-tools/assets/js/analytics_tracker.js?v=1.6', 
     array( 'jquery' ), 
     false, 
     true 
