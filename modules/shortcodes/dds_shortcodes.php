5b07778a1e8b9e1f2e6f744c8f4eb6e2253185bc
Younes Benkheil <younesbenkheil@gmail.com>
2021-09-21 15:51:29 +0200

    make list output

--- modules/shortcodes/dds_shortcodes.php
+++ modules/shortcodes/dds_shortcodes.php
@@ -1,6 +1,19 @@
 <?php
 
-
+function unique_multidim_array($array, $key) { 
+    $temp_array = array(); 
+    $i = 0; 
+    $key_array = array(); 
+    
+    foreach($array as $val) { 
+        if (!in_array($val[$key], $key_array)) { 
+            $key_array[$i] = $val[$key]; 
+            $temp_array[$i] = $val; 
+        } 
+        $i++; 
+    } 
+    return $temp_array; 
+  }
 function prefix_query_args( $query_args, $grid_id ) {
 
     $carousel_cars = get_option("uitgelichtewagens");
@@ -46,6 +59,107 @@ function dds_uitgelichte_wagens( $atts ){
 
 }
 add_shortcode( 'dds_uitgelichte_wagens', 'dds_uitgelichte_wagens' );
+
+
+function dds_aankoop_makes_list( $atts ){
+
+    $output;
+    $args = shortcode_atts( 
+        array(
+            'id'   => ''
+        ), 
+        $atts
+    );
+
+
+    if(!isset( $atts["id"])){
+        return("geen parent id");
+    }
+    else{
+
+        $parent_id = $atts["id"];
+     
+
+        $args = array(
+            'post_type'      => 'page',
+            'posts_per_page' => -1,
+            'post_parent'    => $parent_id,
+            'order'          => 'ASC',
+            'orderby'        => 'menu_order'
+         );
+        
+        
+        $parent = new WP_Query( $args );
+        $makes = array();
+
+        while( $parent->have_posts() ) {
+
+            $post = $parent->the_post();
+
+            $make["name"] = ucwords(get_post_meta(get_the_ID($post),"autoverkopen_merk", true));
+            $make["url"] = get_permalink($post);
+
+            array_push($makes,$make);
+           
+            
+           
+        }
+
+        wp_reset_postdata();
+        
+
+       
+        
+        $makes = unique_multidim_array($makes,"name");
+        
+       
+        sort($makes);
+       
+        $previousLetter = null;
+        $output .= "<style>
+        .dds_aankoop_make_col{
+            width: 25%;
+            padding-bottom: 36px;
+            margin-bottom: 28px;
+            border-bottom: 1px solid #f3f3f3;
+        }
+        .dds_aankoop_make_col a{
+            color: #2f2f2f;
+            font-weight: 400;
+            font-size: 14px;
+            }
+        @media only screen and (max-width: 500px) {
+            .dds_aankoop_make_col{
+                width:50%;
+                }
+          }
+        </style>
+        <div style='display: flex;flex-wrap: wrap;'><div>";
+
+        foreach($makes as $make){
+        
+            
+            if(!empty($make["name"])){
+                $firstLetter = substr($make["name"], 0, 1);
+                if($previous !== $firstLetter){
+                   $output .= "</div><div class='dds_aankoop_make_col'><h2>".$firstLetter."</h2>";
+                }
+
+                $output .=  "<a href='".$make["url"]."'>".$make["name"]."</a><br>";
+               
+            }
+        
+        $previous = $firstLetter;
+        }
+        $output .= "</div></div>";
+        
+        
+
+    }
+    
+    return($output);
+}
+add_shortcode( 'dds_aankoop_makes_list', 'dds_aankoop_makes_list' );
 
 
 
